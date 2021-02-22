1248ce716b1d3a2770b9522c3d16f998edfb30d5
OT <o.trotman5@gmail.com>
2021-02-22 13:31:58 -0400

    Initial Commit

---
+++ TravelRecordAppDemo/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelRecordAppDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+
+            MainPage = new NavigationPage(new MainPage()); 
+            /*a navigation page is necessary in order to manage navigation to and from all pages in the app
+             in order for this page to work a root must be set for it in this case mainpage*/
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ TravelRecordAppDemo/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TravelRecordAppDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LogInButton_Clicked(object sender, EventArgs e)
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text); //returns true or false based on if the text value for emailentry is empty or not
+
+            bool isPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
+
+            if(isEmailEmpty || isPasswordEmpty)
+            {
+
+            }
+            else
+            {
+                Navigation.PushAsync(new HomePage());
+                /*push a new page into the screen, in this case HomePage, see app.xaml.cs*/
+            }
+        }
+    }
+}
